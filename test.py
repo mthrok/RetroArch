f180488c32f1e094cdc8c610a82f468611555134
moto <mthrok@gmail.com>
2016-08-25 21:57:05 -0700

    Update frame format

--- test.py
+++ test.py
@@ -19,12 +19,13 @@ class RAInterface(object):
 
         self.ra = None
 
-        self.frame_data = ctypes.c_void_p()
+        self.frame = None
+        self.frame_pointer = ctypes.c_void_p()
         self.frame_width = ctypes.c_uint()
         self.frame_height = ctypes.c_uint()
         self.frame_pitch = ctypes.c_size_t()
 
-        self.frame_data_ = ctypes.byref(self.frame_data)
+        self.frame_pointer_ = ctypes.byref(self.frame_pointer)
         self.frame_width_ = ctypes.byref(self.frame_width)
         self.frame_height_ = ctypes.byref(self.frame_height)
         self.frame_pitch_ = ctypes.byref(self.frame_pitch)
@@ -40,7 +41,10 @@ class RAInterface(object):
         rarch.init(self.ra, argc, argv)
 
     def step(self):
-        return rarch.step(self.ra)
+        # TODO: Clean up return values
+        ret = rarch.step(self.ra)
+        self.get_frame()
+        return ret
 
     def run(self):
         return rarch.run(self.ra)
@@ -60,7 +64,7 @@ class RAInterface(object):
 
     def get_frame(self):
         rarch.get_frame(
-            self.ra, self.frame_data_,
+            self.ra, self.frame_pointer_,
             self.frame_width_, self.frame_height_,
             self.frame_pitch_
         )
@@ -68,7 +72,7 @@ class RAInterface(object):
         pixel_format = rarch.get_pixel_format(self.ra)
         if pixel_format == 2:
             # RETRO_PIXEL_FORMAT_RGB565
-            data = ctypes.cast(self.frame_data,
+            data = ctypes.cast(self.frame_pointer,
                                ctypes.POINTER(ctypes.c_ushort))
             height = self.frame_height.value
             width = self.frame_width.value
@@ -76,14 +80,16 @@ class RAInterface(object):
             shape = (height, pitch // 2)
             arr = np.ctypeslib.as_array(data, shape=shape)
             arr = arr[:, :width]
-            img = np.zeros((height, width, 3), dtype=np.float)
+            if self.frame is None:
+                shape = (3, height, width)
+                self.frame = np.zeros(shape, dtype=np.uint8)
             # Red
-            img[:, :, 0] = ((arr & 0b1111100000000000) >> 11) / (2 ** 5)
+            self.frame[0, :, :] = ((arr & 0b1111100000000000) >> 11) * 8
             # Green
-            img[:, :, 1] = ((arr & 0b0000011111100000) >> 5) / (2 ** 6)
+            self.frame[1, :, :] = ((arr & 0b0000011111100000) >> 5) * 4
             # Blue
-            img[:, :, 2] = ((arr & 0b0000000000011111) / (2 ** 5))
-            return img
+            self.frame[2, :, :] = (arr & 0b0000000000011111) * 8
+            return self.frame
         else:
             # RETRO_PIXEL_FORMAT_0RGB1555
             # RETRO_PIXEL_FORMAT_XRGB8888
@@ -101,9 +107,8 @@ for i in range(500):
     if frame_count % 400 == 399:
         ra.get_config()
         ra.get_screen_info()
-        frame = ra.get_frame()
-        scipy.misc.toimage(frame, cmin=0, cmax=1.0).save('frame.png')
-        break
+        name = 'frame_{}.png'.format(frame_count)
+        scipy.misc.toimage(ra.frame).save(name)
     if ret == -1:
         break
 ra.stop()
